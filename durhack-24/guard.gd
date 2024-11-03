5ce56b05b45b95b243c955785288075948dd81e9
Simon Ciavarro <115393581+S1m0nCi@users.noreply.github.com>
2024-11-03 10:36:45 +0000

    fix chase issue and add guard

--- durhack-24/guard.gd
+++ durhack-24/guard.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var chase_speed = 25
+var chase_speed = 70
 var track_speed = chase_speed/3
 var player_chase = false
 var player_track = false
@@ -10,14 +10,14 @@ var player = null
 func _physics_process(delta: float) -> void:
 	if player_chase:
 		position += (player.position - position)/chase_speed
-		$AnimatedSprite2D.play("walk_front")
+		$AnimatedSprite2D.play("side")
 		
 		if (player.position.x - position.x) < 0:
 			$AnimatedSprite2D.flip_h = true
 		else:
 			$AnimatedSprite2D.flip_h = false
 	else:
-		$AnimatedSprite2D.play("idle_front")
+		$AnimatedSprite2D.play("front")
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body # player is the body that has entered
